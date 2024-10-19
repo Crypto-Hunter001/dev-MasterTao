2af2b4429ffb112f29777848e696f4a9731dadb1
Nazar Krughliakov <zonnix336@gmail.com>
2024-10-19 20:00:42 +0300

    delivery desktop/mobile done

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -13,12 +13,12 @@ const swiper = new Swiper('#banner-swiper', {
     enabled: false,
   },
 
-  breakpoints:{
+  breakpoints: {
     992: {
       navigation: {
         enabled: true,
-       nextEl: '.swiper-button-next',
-       prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
     },
   }
@@ -26,43 +26,136 @@ const swiper = new Swiper('#banner-swiper', {
 });
 
 
-
 const typeFile = document.querySelectorAll('.custom-file')
 
+
 typeFile.forEach(item => {
-    item.addEventListener('change', () => {
-        const fileName = item.closest('.input-box').querySelector('.file-name')
-        fileName.innerHTML = item.files[0].name;
-    })
+  item.addEventListener('change', () => {
+    const fileName = item.closest('.input-box').querySelector('.file-name')
+    fileName.innerHTML = item.files[0].name;
+  })
 })
 
 const andvantageSlide = document.querySelectorAll('.advantages .swiper-slide').length
 
 const swiperAdvantages = new Swiper('#advantages', {
+  // Optional parameters
+  loop: true,
+  simulateTouch: true,
+  slidesPerView: 1,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    enabled: true,
+  },
+
+  breakpoints: {
+  990: {
+      spaceBetween: 0,
+      loop: false,
+      slidesPerGroup: andvantageSlide,
+      simulateTouch: false,
+      pagination: {
+        el: '.swiper-pagination',
+        enabled: false,
+      },
+
+    },
+  }
+
+
+
+
+
+
+
+
+
+
+});
+
+const swiperSlideTemplate = new Swiper('.swiper-template', {
 	// Optional parameters
 	loop: true,
 	simulateTouch: true,
+	speed: 400,
 	slidesPerView: 1,
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-		enabled: true,
+	clickable: true,
+
+	navigation: {
+		enabled: false,
+		nextEl: '.swiper-button-next-unique',
+		prevEl: '.swiper-button-prev-unique',
 	},
 
+	pagination: {
+		el: '.swiper-pagination',
+		enabled: true,
+		clickable: true,
+	},
+
+	// Navigation arrows
+
+
 	breakpoints: {
-		1025: {
-			spaceBetween: 0,
-			loop: false,
-			slidesPerGroup: andvantageSlide,
-			simulateTouch: false,
+
+		767: {
+			navigation: {
+				enabled: true,
+				nextEl: '.swiper-button-next-unique',
+				prevEl: '.swiper-button-prev-unique',
+
+			},
+
+			pagination: {
+				el: '.swiper-pagination',
+				enabled: false,
+			},
+		},
+
+
+		861: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+
+			navigation: {
+				enabled: true,
+				nextEl: '.swiper-button-next-unique',
+				prevEl: '.swiper-button-prev-unique',
+
+			},
+
 			pagination: {
 				el: '.swiper-pagination',
 				enabled: false,
 			},
 
+
+
+		},
+
+
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+
+			navigation: {
+				enabled: true,
+				nextEl: '.swiper-button-next-unique',
+				prevEl: '.swiper-button-prev-unique',
+			},
+
+			pagination: {
+				el: '.swiper-pagination',
+				enabled: false,
+			},
+
+
 		},
 	}
+
+
 
 
 
