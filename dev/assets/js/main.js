20a497014733ca09ae8b040622bdc00ec624ab91
Nazar Krughliakov <zonnix336@gmail.com>
2024-10-21 01:37:57 +0300

    list desktop done

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -75,88 +75,175 @@ const swiperAdvantages = new Swiper('#advantages', {
 
 });
 
-const swiperSlideTemplate = new Swiper('.swiper-template', {
+// const swiperSlideTemplate = new Swiper('.swiper-template', {
+// 	// Optional parameters
+// 	loop: true,
+// 	simulateTouch: true,
+// 	speed: 400,
+// 	slidesPerView: 1,
+// 	clickable: true,
+
+// 	navigation: {
+// 		enabled: false,
+// 		nextEl: '.swiper-button-next-unique',
+// 		prevEl: '.swiper-button-prev-unique',
+// 	},
+
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		enabled: true,
+// 		clickable: true,
+// 	},
+
+// 	// Navigation arrows
+
+
+// 	breakpoints: {
+
+// 		767: {
+// 			navigation: {
+// 				enabled: true,
+// 				nextEl: '.swiper-button-next-unique',
+// 				prevEl: '.swiper-button-prev-unique',
+
+// 			},
+
+// 			pagination: {
+// 				el: '.swiper-pagination',
+// 				enabled: false,
+// 			},
+// 		},
+
+
+// 		861: {
+// 			slidesPerView: 2,
+// 			spaceBetween: 30,
+
+// 			navigation: {
+// 				enabled: true,
+// 				nextEl: '.swiper-button-next-unique',
+// 				prevEl: '.swiper-button-prev-unique',
+
+// 			},
+
+// 			pagination: {
+// 				el: '.swiper-pagination',
+// 				enabled: false,
+// 			},
+
+
+
+// 		},
+
+
+// 		1210: {
+// 			slidesPerView: 3,
+// 			spaceBetween: 30,
+
+// 			navigation: {
+// 				enabled: true,
+// 				nextEl: '.swiper-button-next-unique',
+// 				prevEl: '.swiper-button-prev-unique',
+// 			},
+
+// 			pagination: {
+// 				el: '.swiper-pagination',
+// 				enabled: false,
+// 			},
+
+
+// 		},
+// 	}
+
+
+
+
+
+// });
+
+
+const swiperTemplate = document.querySelectorAll('.swiper-template')
+const sliderTemplatePrevArrow = document.querySelectorAll('.swiper-button-prev-unique')
+const sliderTemplateNextArrow = document.querySelectorAll('.swiper-button-next-unique')
+
+
+
+sliderTemplatePrevArrow.forEach((arrow, index) => {
+    arrow.classList.add('swiper-button-prev-unique-' + index)
+})
+
+sliderTemplateNextArrow.forEach((arrow, index) => {
+    arrow.classList.add('swiper-button-next-unique-' + index)
+})
+
+swiperTemplate.forEach((item, index) => {
+    
+  new Swiper(item, {
+
 	// Optional parameters
 	loop: true,
-	simulateTouch: true,
 	speed: 400,
 	slidesPerView: 1,
-	clickable: true,
 
-	navigation: {
-		enabled: false,
-		nextEl: '.swiper-button-next-unique',
-		prevEl: '.swiper-button-prev-unique',
-	},
-
-	pagination: {
+    	pagination: {
 		el: '.swiper-pagination',
 		enabled: true,
 		clickable: true,
 	},
 
-	// Navigation arrows
+
+	navigation: {
+		enabled: false,
+		nextEl: '.swiper-button-next-unique-' + index,
+		prevEl: '.swiper-button-prev-unique-' + index,
+	},
+
 
 
 	breakpoints: {
 
 		767: {
+            slidesPerView: 1,
+
 			navigation: {
 				enabled: true,
-				nextEl: '.swiper-button-next-unique',
-				prevEl: '.swiper-button-prev-unique',
+				nextEl: '.swiper-button-next-unique-' + index,
+				prevEl: '.swiper-button-prev-unique-' + index,
 
-			},
-
-			pagination: {
-				el: '.swiper-pagination',
-				enabled: false,
 			},
 		},
-
-
 		861: {
 			slidesPerView: 2,
 			spaceBetween: 30,
 
 			navigation: {
 				enabled: true,
-				nextEl: '.swiper-button-next-unique',
-				prevEl: '.swiper-button-prev-unique',
+				nextEl: '.swiper-button-next-unique-' + index,
+				prevEl: '.swiper-button-prev-unique-' + index,
 
 			},
-
 			pagination: {
-				el: '.swiper-pagination',
 				enabled: false,
 			},
-
-
-
 		},
-
-
 		1210: {
 			slidesPerView: 3,
 			spaceBetween: 30,
-
 			navigation: {
 				enabled: true,
-				nextEl: '.swiper-button-next-unique',
-				prevEl: '.swiper-button-prev-unique',
+				nextEl: '.swiper-button-next-unique-' + index,
+				prevEl: '.swiper-button-prev-unique-' + index,
 			},
 
 			pagination: {
-				el: '.swiper-pagination',
 				enabled: false,
 			},
 
 
 		},
 	}
-
-
-
-
-
 });
+})
+
+
+
